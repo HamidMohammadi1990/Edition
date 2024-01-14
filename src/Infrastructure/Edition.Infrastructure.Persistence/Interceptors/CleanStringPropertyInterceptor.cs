9c6e148067877fff45a44295e7087a1682d37a83
HamidMohammadi1990 <75840339+HamidMohammadi1990@users.noreply.github.com>
2024-01-14 18:25:47 +0330

    Update CleanStringPropertyInterceptor.cs

--- src/Infrastructure/Edition.Infrastructure.Persistence/Interceptors/CleanStringPropertyInterceptor.cs
+++ src/Infrastructure/Edition.Infrastructure.Persistence/Interceptors/CleanStringPropertyInterceptor.cs
@@ -17,8 +17,7 @@ public class CleanStringPropertyInterceptor : SaveChangesInterceptor
         {
             if (item.Entity == null)
                 continue;
-
-            // test
+            
             var properties = item.Entity.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance)
                 .Where(p => p.CanRead && p.CanWrite && p.PropertyType == typeof(string));
 
